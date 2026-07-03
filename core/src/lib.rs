d7925d48169a7a18d315c2afbc48f4c118c787bf
EmIsGreat <tim.mayr@gmx.at>
2026-07-03 02:16:47 +0200

    added unsafe op lint

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,6 +3,7 @@
 #![deny(clippy::expect_used)]
 #![deny(clippy::panic)]
 #![deny(unused_must_use)]
+#![deny(unsafe_op_in_unsafe_fn)]
 
 //! # Monsoon Core
 //!
--- db/src/lib.rs
+++ db/src/lib.rs
@@ -2,6 +2,6 @@
 #![deny(clippy::expect_used)]
 #![deny(clippy::panic)]
 #![deny(unused_must_use)]
-
+#![deny(unsafe_op_in_unsafe_fn)]
 pub mod db_provider;
 pub mod manifest;
